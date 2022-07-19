66d28045b6fc34105001b8322d58917e95b02f8e
Dhruvesh Patel <dhruveshpatel2004@gmail.com>
2022-07-19 17:06:08 +0530

    change link in home.html

--- Car_indexpage.js
+++ Car_indexpage.js
@@ -2,11 +2,11 @@ const user = document.getElementById("Username")
 const pass = document.getElementById("Password")
 document.querySelector("#Signinbtn").addEventListener("click", () => {
     if (user.value == "Dhruvesh" && pass.value == "dhruvesh.123") {
-        window.location.href = "index.html"
+        window.location.href = "home.html"
         alert("Welcome DhruveshPatel");
     }
     else if (user.value == "Dhruv" && pass.value == "dhruv.123") {
-        window.location.href = "index.html"
+        window.location.href = "home.html"
         alert("Welcome DhruvThakkar");
     }
     else {
